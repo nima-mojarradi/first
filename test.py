9c75753e0413015aa393ce9c7ca36ad823bec4c8
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-06-02 15:08:40 +0430

    this is my new file in vscode

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-print("hello")
-print("world")
+import re
+password = input("enter your password: ")
+def check_password(password):
+    pattern = ".*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])"
+    result = re.search(pattern, password)
+    if (result):
+        print("your password is valid password")
+    else:
+        print("this is not a valid password")
+
+check_password(password)
